fda53e295e5dece4cc8736919d958261973e9424
Andrew Storch <astorch13@gmail.com>
2020-08-05 15:11:13 -0400

    finished tour of heroes

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -11,6 +11,18 @@ import { MessageService } from '../message.service';
 export class HeroesComponent implements OnInit {
   heroes: Hero[];
   constructor(private heroService: HeroService, private messageService: MessageService) { }
+  add(name: string): void {
+    name = name.trim();
+    if (!name) { return; }
+    this.heroService.addHero({ name } as Hero)
+      .subscribe(hero => {
+        this.heroes.push(hero);
+      });
+  }
+  delete(hero: Hero): void {
+    this.heroes = this.heroes.filter(h => h !== hero);
+    this.heroService.deleteHero(hero).subscribe();
+  }
   getHeroes(): void{
     this.heroService.getHeroes().subscribe(heroes => this.heroes = heroes);
   }
